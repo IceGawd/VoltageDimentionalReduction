623b91de10f14d2005bb59d3e3cc574ea68d8469
Yoav Freund <yfreund@ucsd.edu>
2025-05-09 16:00:01 -0700

    Added sketch of top level documentation

---
+++ code/__init__.py
@@ -0,0 +1,23 @@
+"""The VoltageDimentionalReduction package is an implementation of
+the voltage-based cloud-point mapping software
+
+The code is organized as follows. The main classes are the problem class which defines a partition graph and the landmarks. THe local ...
+
+
+The module/class Hierarchy is (top level modules (directories), divided into submodules (.py files), divided into classes) each one line of description. At this time there seems no need for directory-level modules. So the list should include just two levels: .py files and Classes. Functions should all be methods, i.e. part of a directory.
+
+Follw the directions in https://realpython.com/documenting-python-code/#documenting-your-python-code-base-using-docstrings
+
+
+Current list of .py files is below. Consolidate into files/classes
+with well defined roles.
+
+bpf.py  - Finds the best parameters: Kernel width aand resistance to groun
+create_data.py - 
+kmeans.py - Creates the partitioning 
+localvoltagesolver.py 
+meanshiftclustering.py
+mnist.py
+timemeasurement.py
+voltage.py
+"""
