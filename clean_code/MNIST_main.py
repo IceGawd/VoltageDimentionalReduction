043f33c9df2203b63e01ef2229eb819872571ef5
IceGawd <avighna.kukreja@gmail.com>
2025-06-11 17:10:08 -0700

    Optimizer

--- clean_code/MNIST_main.py
+++ clean_code/MNIST_main.py
@@ -25,36 +25,35 @@ import setofpoints
 # yoav: TODO: read points using streaming Kmeans
 
 ## Loading mnist data, no kmeans
-print("Loading Data...")
 mnist = fetch_openml('mnist_784', version=1, as_frame=False)
 X, y = mnist.data, mnist.target.astype(np.int64)
 
 # Normalize pixel values to [0, 1]
 X = X / 255.0
 
-#av: TODO replace compressed_set with point_set
 # define set of points on which we will work
-compressed_set = setofpoints.SetOfPoints(points=X[:1000])
+point_set = setofpoints.SetOfPoints(points=X[:1000])
 
 #av: TODO choose landmarks randoomly or greeedily
-#Special for MNist : 
+# Special for MNist : 
 # Select one sample per digit to serve as a landmark
 landmarks = []
 for digit in range(10):
-    indices = np.where(y == digit)[0]
-    # Choose the first occurrence as the landmark
-    landmarks.append(landmark.Landmark.createLandmarkClosestTo(compressed_set, X[indices[0]], 1))
+	indices = np.where(y == digit)[0]
+	# Choose the first occurrence as the landmark
+	landmarks.append(landmark.Landmark.createLandmarkClosestTo(point_set, X[indices[0]], 1))
 
-##av: todo add method to Problem that find a good r using binary search
-mnist_problem = problem.Problem(compressed_set, r=1)
+mnist_problem = problem.Problem(point_set, r=1)
+mnist_problem.optimize(landmarks, k=2, r=0.5)
 
 # Initialize the map
 voltage_map = voltagemap.VoltageMap()
 
 # Compute voltages for each landmark and store in the map
 for lm in landmarks:
-    mnist_solver = solver.Solver(problem=mnist_problem)
-    voltages = mnist_solver.compute_voltages(k=2, landmarks=[lm])
-    voltage_map.add_solution(landmark_index=lm.index, voltages=voltages)
+	mnist_solver = solver.Solver(problem=mnist_problem)
+	voltages = mnist_solver.compute_voltages(k=2, landmarks=[lm])
+	voltage_map.add_solution(landmark_index=lm.index, voltages=voltages)
 
 #av: TODO call some visualizations, that store the figure into a file.
+visualization.Visualization.plot_mds_digits([2, 3, 4, 5, 7, 8, 9], voltage_map, point_set, y[:1000], alpha_actual=0.5, out_file="../inputoutput/matplotfigures/mnist_mds.png")
