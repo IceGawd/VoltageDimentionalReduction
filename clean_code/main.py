35785eea7deef8954ea2e1e0aef9adc960b44ca7
Yoav Freund <yfreund@ucsd.edu>
2025-06-28 22:20:56 -0700

    Working on finding landmarks

--- clean_code/main.py
+++ clean_code/main.py
@@ -23,7 +23,7 @@ import setofpoints
 import kmeans
 import config
 
-def test_voltage(voltages, ignore_fraction:float=0.95, thr:float=0.05):
+def test_voltage(voltages, ignore_fraction:float=0.50, thr:float=0.09):
 	sorted = np.sort(voltages.flatten())
 	low=int(ignore_fraction*sorted.shape[0])
 	scaled = (sorted - sorted[low] )/ (sorted[-1] - sorted[low])
@@ -56,89 +56,93 @@ def compute_distances(point_set, voltages):
 
 	# 2. Distance based on voltages
 	X= voltages
-	index = faiss.IndexFlatL2(X.shape[1])  # L2 distance index
-	index.add(X.astype(np.float32))  # Add points to the index
-	D2, _ = index.search(X.astype(np.float32), X.shape[0])
+	index = faiss.IndexFlatL2(voltages.shape[1])  # L2 distance index
+	index.add(voltages.astype(np.float32))  # Add points to the index
+	D2, _ = index.search(voltages.astype(np.float32), voltages.shape[0])
 	np.fill_diagonal(D2, -np.inf)
+
+	return D1,D2
 
 	# 3. Distance based on k-connectivity graph
 	# Using the point_set directly
 	# Create a k-nearest neighbors graph
-	k = config.params['k']
-	from scipy.sparse import lil_matrix
-	from sklearn.neighbors import NearestNeighbors
+	#k = config.params['k']
+	#from scipy.sparse import lil_matrix
+	#from sklearn.neighbors import NearestNeighbors
 
-	nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(X)
-	_, indices = nbrs.kneighbors(X)
+	#nbrs = NearestNeighbors(n_neighbors=k, algorithm='auto').fit(X)
+	#_, indices = nbrs.kneighbors(X)
 	# Create a sparse adjacency matrix
-	n = X.shape[0]
-	adjacency_matrix = lil_matrix((n, n), dtype=np.float32)
-	for i in range(n):
-		for j in indices[i]:
-			if i != j:
-				adjacency_matrix[i, j] = 1.0
+	#n = X.shape[0]
+	#adjacency_matrix = lil_matrix((n, n), dtype=np.float32)
+	#for i in range(n):
+	#	for j in indices[i]:
+	#		if i != j:
+	#			adjacency_matrix[i, j] = 1.0
 
 
-
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
+	# Load configuration parameters
 	#config.params['file_path']= '../data/glove/shuffled_output.txt'
 	#config.params['split_char']= ' '
 	#config.params['normalize_vecs']= True
 
-	config.params['file_path']= '../data/mnist/mnist.csv'
+	config.params['file_path']= '../../Voltage_Data/mnist/mnist.csv'
 	config.params['split_char']= ','
 	config.params['normalize_vecs']= False
 
 	config.params['max_centroids']= 1000
 	config.params['init_size']= 5000
 	config.params['batch_size']= 1000
-	config.params['output']= 'streaming_centroids.npy'
+	config.params['kmeans_output']= '../../Voltage_Temp/Results/streaming_centroids.npy'
+	config.params['Voltage_map_output']= '../../Voltage_Temp/Results/voltage_map.npy'
 	config.params['k']=10
 
-	# generate centroids using streaming k-means
-#	centroids, counters, majority_labels, _,_=kmeans.Streaming_Kmeans(config.params['file_path'])
+	run_kmeans=False
 
-#	X=np.stack(centroids)
-#	print('X.shape=',X.shape)
-#	# Normalize pixel values to [0, 1]
-#	X = X / 255.0   #for visualization purposes
-#	y= np.array(majority_labels)
+	workspace_file="../../Voltage_Temp/Intermediates/pointset.pkl"
+	import dill  # dill is used to save the workspace
 
-#	# define set of points on which we will work
-#	point_set = setofpoints.SetOfPoints(points=X, weights=counters)
+	if run_kmeans:
+		# generate centroids using streaming k-means
+		centroids, counters, majority_labels, _,_=kmeans.Streaming_Kmeans(config.params['file_path'])
 
-	import pickle
-#	with  open('../../Voltage_Temp/Intermediates/pointset.pkl','wb') as pkl:
-#		pickle.dump(point_set,pkl)
-	with  open('../../Voltage_Temp/Intermediates/pointset.pkl','rb') as pkl:
-		point_set=pickle.load(pkl)
+		X=np.stack(centroids)
+		# Normalize pixel values to [0, 1]
+		X = X / 255.0   #for visualization purposes
+		y= np.array(majority_labels)
 
-	print('started')
-	#choose landmarks one at a time, starting with a random centroid and then choosing a centroid where all of the voltages so far are low.
+		# define set of points on which we will work
+		point_set = setofpoints.SetOfPoints(points=X, weights=counters)
 
-	import random
 
+### Store /recover intermediate workspace
+		dill.dump_session(workspace_file)
+	else:
+		dill.load_session(workspace_file)
+	
+	print("starting after kmeans is done")
+	   
 	j=0
 	# Initialize the map
 	landmarks=[]
 	voltage_map = voltagemap.VoltageMap()
-	problem = problem.Problem(point_set)
-	solver=solver.Solver(problem)
+	_problem = problem.Problem(point_set)
+	_solver=solver.Solver(_problem)
 	max_voltage=np.zeros(point_set.__len__())
 
-	Voltage_thr=0.1     # maximal voltage for adding a landmark
+	Voltage_thr=0.2     # maximal voltage for adding a landmark
 	coverage_threshold=0.95  # minimal coverage to terminate the program
 
+	import random
 	while True:
 		index=random.randint(0, len(point_set)-1)  # choose a random point
 		if(max_voltage[index]>Voltage_thr):  # check whether it already has significant voltage
 			continue
 		# choose next landmark to add
 		candidate_landmark=landmark.Landmark(index,voltage=1.0)
-		voltages=solver.compute_voltages(candidate_landmark)
-		advantage,test_passed, scaled_voltages = test_voltage(voltages) 
+		voltages=_solver.compute_voltages(candidate_landmark)
+		advantage,test_passed, scaled_voltages = test_voltage(voltages,ignore_fraction=0.5, thr=0.05) 
 		if not test_passed:
 			continue
 
@@ -152,7 +156,7 @@ if __name__=="__main__":
 		max_voltage=np.max(voltages_so_far,axis=0)
 		aver_max=np.mean(max_voltage.flatten())
 		min_max=np.min(max_voltage.flatten())
-		non_zero_fraction =  np.mean(max_voltage.flatten()>0)
+		non_zero_fraction =  np.mean(max_voltage.flatten()>Voltage_thr)
 		print(f"iter={j}, advantage={advantage},\
 		 aver_max={aver_max}, min_max={min_max},\
 			non_zero_fraction={non_zero_fraction}")
@@ -160,13 +164,21 @@ if __name__=="__main__":
 			print(f'non_zero_fraction >= {coverage_threshold}, Stopping')
 			break
 
+	print(f"voltages_so_far.shape={voltages_so_far.shape}, landmarks={len(landmarks)}")
+	print(f"point_set.shape={point_set.points.shape}, weights={point_set.weights.shape}")
 	#call a function for computing distances between all poirs of points in pointset
-	distances = compute_distances(point_set,voltages_so_far)
-
+	Deuclid,Dvolt = compute_distances(point_set,voltages_so_far.T)
+	print(f"Deuclid.shape={Deuclid.shape}, Dvolt.shape={Dvolt.shape}")
 
 
 	# save the workspace for later use
-	import dill  # or use 'pickle' for simpler objects
-	with open("../../Voltage_Temp/Intermediates/workspace.pkl", "wb") as f:
-		dill.dump_session(f)
-	print("main complete. Workspace saved to '../../Voltage_Temp/Intermediates/workspace.pkl'.")
+
+	workspace_file="../../Voltage_Temp/Intermediates/workspace.pkl"
+	dill.dump_session(workspace_file)
+	print(f"Workspace saved to {workspace_file}")
+
+	import pickle
+	with open(config.params['Voltage_map_output'], 'wb') as f:
+		pickle.dump(voltage_map, f)
+	print(f"Voltage map saved to {config.params['Voltage_map_output']}")
+
