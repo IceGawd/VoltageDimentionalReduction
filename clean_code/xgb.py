34e77c9781a16eb94849ddb0ac698466b7fd1e31
Yoav Freund <yfreund@ucsd.edu>
2025-07-11 15:40:54 -0700

    updating select_landmarks and xgb

--- clean_code/xgb.py
+++ clean_code/xgb.py
@@ -75,7 +75,8 @@ def train_and_evaluate(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     model = XGBClassifier(n_estimators=1000,
                           learning_rate=0.1,
-                           max_depth=2,
+                          objective='multi:softmax', num_class=10,
+                           max_depth=5,
                            eval_metric='merror',  # specify metric
                            random_state=42)
     
