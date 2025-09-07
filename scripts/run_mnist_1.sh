5ba3fe9e954c2276942c007e7f8b8e4108d41937
Yoav Freund <yfreund@ucsd.edu>
2025-09-07 13:53:27 -0700

    working on scripts

--- scripts/run_mnist_1.sh
+++ scripts/run_mnist_1.sh
@@ -1,9 +1,7 @@
-python ../clean_code/main.py \
+python run_main_select_visualize.py \
         ../../Voltage_Data/mnist/mnist.csv\
         --split_char ',' \
-        --save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl
-
-python ../clean_code/select_landmarks_MI.py \
+        --save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
        --NoOfLandmarks 10\
        --save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl
 
