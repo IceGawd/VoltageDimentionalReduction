bb2c576c6cab360d2f1462d7d8e708fdcdaafaae
Yoav Freund <yfreund@ucsd.edu>
2025-09-17 13:28:05 -0700

    changed run_mnist

--- scripts/run_mnist.sh
+++ scripts/run_mnist.sh
@@ -1,19 +1,21 @@
-set -e
+#set -e
 
 # python ../clean_code/main.py \
 # 	../../Voltage_Data/mnist/mnist.csv\
-# 	--max_centroids 2000 \
+# 	--max_centroids 100 \
 # 	--batch_size=10000 \
 # 	--split_char ',' \
 # 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl
 
 # python ../clean_code/select_landmarks_MI.py \
-# 	--NoOfLandmarks 20 \
+# 	--NoOfLandmarks 10 \
 # 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl
 
 python ../clean_code/Visualization/visualizations.py \
-	../../Voltage_Data/mnist/mnist.csv\
 	--scatter_element digit \
 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
+	--point_from_file ../../Voltage_Data/mnist/mnist.csv \
+	--plotted_points 2000 \
+	--remove_clutter \
 	--plot_dir ../../Voltage_Temp/Scatter_Plots/mnist/ \
 	--dpi 300
