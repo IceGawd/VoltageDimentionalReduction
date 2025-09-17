b2ed8b85b8b9ba2f57f74d337055d01d4755c9b2
Yoav Freund <yfreund@ucsd.edu>
2025-09-16 17:06:45 -0700

    new run_mnist.sh, crashes because of points_from_files

--- scripts/run_mnist.sh
+++ scripts/run_mnist.sh
@@ -1,33 +1,19 @@
-set -e
+# python ../clean_code/main.py \
+#        ../../Voltage_Data/mnist/mnist.csv\
+#        --max_centroids 100 \
+#        --batch_size=10000 \
+#         --split_char ',' \
+#         --save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl
 
-# python3 ../clean_code/main.py \
-# 	../../Voltage_Data/mnist/mnist.csv \
-# 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
-# 	--split_char ',' \
+# python ../clean_code/select_landmarks_MI.py \
+#        --NoOfLandmarks 10\
+#        --save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl
 
-# python3 ../clean_code/select_landmarks_MI.py \
-# 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
-# 	--NoOfLandmarks 10 \
-
-python3 ../clean_code/Visualization/visualizations.py \
-	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
-	--plot_dir ../../Voltage_Temp/Scatter_Plots/mnist/centroid \
- 	--pad_pixels 0 \
- 	--distinct_colors \
-	--remove_clutter
-
-# python3 ../clean_code/Visualization/visualizations.py \
-# 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
-# 	--plot_dir ../../Voltage_Temp/Scatter_Plots/mnist/point \
-# 	--point_from_file ../../Voltage_Data/mnist/mnist.csv \
-# 	--dpi 300 \
-# 	--percent_size 0.005
-
-# python3 ../clean_code/Visualization/visualizations.py \
-# 	--save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
-# 	--plot_dir ../../Voltage_Temp/Scatter_Plots/mnist/pointclutterless \
-# 	--point_from_file ../../Voltage_Data/mnist/mnist.csv \
-# 	--dpi 300 \
-# 	--percent_size 0.005 \
-# 	--pad_pixels 0 \
-# 	--remove_clutter
+python ../clean_code/Visualization/visualizations.py \
+       ../../Voltage_Data/mnist/mnist.csv\
+#       --indices 1,2,3,4,5,6,7,8,9,0\
+       --point_from_file ../../Voltage_Data/mnist/mnist.csv\
+	--plotted_points 1000\
+       --scatter_element digit \
+       --save_data ../../Voltage_Temp/Results/mnist/saved_data.pkl \
+       --plot_dir ../../Voltage_Temp/Scatter_Plots/mnist/
